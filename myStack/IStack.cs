62ef9d32b0a52eba4932406d90bfb03627c609ec
gabri <gabrielemanuzzato@gmail.com>
2026-03-20 11:30:52 +0100

    Aggiungere i file di progetto.

---
+++ myStack/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myStack
+{
+    internal interface IStack<T>
+    {
+        public void Push(T Data);
+        public T Pop();
+        public T? Peek();
+        public void Clear();
+        public bool IsEmpty();
+    }
+}
---
+++ myStack/Stack.cs
@@ -0,0 +1,69 @@
+﻿using myStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAndStack
+{
+    internal class Stack<T> : IStack<T>
+    {
+        private Node<T>? top;
+
+        public int Count;
+
+        public Stack()
+        {
+            top = null;
+        }
+
+        public void Push(T Data)
+        {
+            Node<T>? newNode = new Node<T>(Data);
+            
+            if (top == null)
+            {
+                top = newNode;
+                Count++;
+                return;
+            }
+
+            newNode.Next = top;
+            top = newNode;
+
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (top == null)
+                throw new Exception("Pila non valorizzata");
+            
+            T Data = top.Data;
+            top = top.Next;
+            Count--;
+
+            return Data;
+        }
+
+        public T? Peek()
+        {
+            if (top == null)
+                throw new Exception("");
+            
+            return top.Data;
+        }
+
+        public void Clear()
+        {
+            top = null;
+            Count = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+    }
+}
